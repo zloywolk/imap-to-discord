2a855651f2a12c53d4ef92dd3b502c1d5827408c
Patrick Sachs <kontakt@patrick-sachs.de>
2022-04-24 12:48:13 +0200

    Added: `Append`, `Prepend` and `Replace` option for default formatter

--- src/formatter/default.ts
+++ src/formatter/default.ts
@@ -3,37 +3,75 @@ import Formatter from "./formatter";
 import config from '../config';
 import Thing from "../thing/thing";
 
+interface Include {
+  id: string;
+  content: boolean;
+  replace: string;
+  append: string;
+  preprend: string;
+}
+
 export default class DefaultFormatter extends Formatter {
-  private includeSubject!: boolean;
-  private includeSender!: boolean;
-  private includeBody!: boolean;
-  private includeServer!: boolean;
+  private includes!: Record<string, Include>;
 
   init() {
-    this.includeSubject = config('IncludeSubject', true, this.options);
-    this.includeSender = config('IncludeSender', true, this.options);
-    this.includeBody = config('IncludeBody', true, this.options);
-    this.includeServer = config('IncludeServer', true, this.options);
+    this.includes = {
+      ['Subject']: this.readInclude('Subject'),
+      ['Sender']: this.readInclude('Sender'),
+      ['Body']: this.readInclude('Body'),
+      ['Server']: this.readInclude('Server'),
+    };
   }
 
-  format(thing: Thing) {
-    const md = this.md(thing.get<string>('content'));
+  readInclude(id: string): Include {
+    return {
+      id,
+      content: config('Include' + id, true, this.options),
+      append: config('Append' + id, '', this.options),
+      preprend: config('Prepend' + id, '', this.options),
+      replace: config('Replace' + id, '', this.options),
+    }
+  }
+
+  async runInclude(id: string, setFn: (str: string) => Promise<MessageBuilder>, readFn: () => Promise<string | undefined>) {
+    const include = this.includes[id];
+    let str: undefined | string;
+    if (include.replace) {
+      str = include.replace;
+    } else if (include.content) {
+      str = await readFn();
+    }
+    if (str || include.preprend || include.append) {
+      await setFn(this.limit(str, {
+        prefix: include.preprend,
+        suffix: include.append,
+      }));
+    }
+  }
+
+  async format(thing: Thing) {
     const sourceThing = thing.get<Thing>('source-thing');
-    const origin = thing.get<any[]>('origin');
-    let builder = new MessageBuilder();
-    if (this.includeSubject) {
-      builder = builder.setTitle(this.limit(thing?.get<string>('name')));
-    }
-    if (this.includeSender) {
-      builder = builder.setAuthor(this.limit(this.address(origin)));
-    }
-    if (this.includeBody) {
-      builder = builder.setDescription(this.limit(md));
-    }
-    if (this.includeServer) {
-      const server = `${sourceThing?.get<string>('name')}: ${sourceThing?.get<string>('user')} (${sourceThing?.get<string>('collection')})`;
-      builder = builder.setFooter(this.limit(server));
-    }      
+    const builder = new MessageBuilder();
+    await this.runInclude(
+      'Subject',
+      async subject => builder.setTitle(subject),
+      async () => thing?.get<string>('name'),
+    );
+    await this.runInclude(
+      'Sender',
+      async sender => builder.setAuthor(sender),
+      async () => this.address(thing.get<any[]>('origin')),
+    );
+    await this.runInclude(
+      'Body',
+      async body => builder.setDescription(body),
+      async () => this.md(thing.get<string>('content')),
+    );
+    await this.runInclude(
+      'Server',
+      async server => builder.setFooter(server),
+      async () => `${sourceThing?.get<string>('name')}: ${sourceThing?.get<string>('user')} (${sourceThing?.get<string>('collection')})`,
+    );  
     return builder;
   }
 }
